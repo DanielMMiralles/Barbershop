f5cd590ef855ccc92c9fa50179971c058edde2c1
AlvaroGA <alvarogutierrezaboul@gmail.com>
2025-03-28 15:15:38 -0400

    adapatable y responsivo centro de informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import { MdPerson } from "react-icons/md"
 import { FaFacebook } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
@@ -10,28 +10,44 @@ import {NavBar} from "./navBar.jsx";
 
 export function App () {
     const [fechaCita, setFechaCita] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex1, setCurrentIndex2] = useState(0);
+    const descriptions = [
+        "Más que una barbería, somos un espacio donde la tradición se encuentra con la modernidad. Aquí, cada corte y cada afeitado es una obra maestra, diseñada para resaltar tu personalidad y hacerte sentir impecable. Nuestro equipo de expertos barberos, apasionados por su oficio, te brindará una experiencia única y personalizada. ¡Ven y descubre el arte de la barbería!",
+        'Ofrecemos una amplia gama de servicios para satisfacer todas tus necesidades de cuidado personal. Desde cortes 1  de cabello clásicos y modernos hasta afeitados tradicionales con toalla caliente, arreglo y diseño de barba, tratamientos capilares, coloración de cabello y barba, cortes y diseños para niños, masajes faciales y depilación facial con cera o hilo. Nos aseguramos de utilizar productos de alta calidad y técnicas innovadoras para garantizar resultados excepcionales',
+        'Nos encontramos ubicados en Lechería, Anzoátegui, Venezuela, específicamente en la Avenida Principal, Centro Comercial Plaza Mayor, Local 12, Lechería 2101, Anzoátegui, Venezuela. Nuestra barbería está situada en el corazón de Lechería, dentro del reconocido Centro Comercial Plaza Mayor, lo que facilita el acceso desde la Avenida Principal y ofrece estacionamiento disponible para nuestros clientes. Además, estamos rodeados de restaurantes, tiendas y otros servicios, lo que te permite combinar tu visita a la barbería con otras actividades. Nuestro ambiente es moderno y acogedor, con una decoración inspirada en las barberías clásicas y toques contemporáneos, y contamos con equipamiento de última generación para brindarte un servicio excepcional.'
+    ]
+    const barbeOps = [
+        'Bienvenidos a "Nombre de la Barbería" - Tu estilo, nuestro arte',
+        'Servicios que ofrecemos',
+        'Nuestra ubicacion'
+    
+    ]
+    useEffect(() => { //FUNCION QUE CALCULA TIEMPO ENTRE CAMBIO DE TEXTOS
+        const interval = setInterval(() => {
+        setCurrentIndex2((prevIndex) => (prevIndex + 1) % descriptions.length)
+        }, 3000)
+        return () => clearInterval(interval) }, [])
+
+    useEffect(() => { //FUNCION QUE CALCULA TIEMPO ENTRE CAMBIO DE TEXTOS
+            const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % barbeOps.length)
+            }, 3000)
+            return () => clearInterval(interval) }, [])
+    
     return(
         <div>
-            {/* NavBar component */}
+            {/* NavBar componente */}
             <NavBar />
             <br/><br/><br/><br/>
             <section>
-           <div className="contenedor">
-                <div className="Descripcion">
-                    <h2>aBienvenidos a "Nombre de la Barbería" - Tu estilo, nuestro arte</h2><br />
-                    <p>En "Nombre de la Barbería", donde la elegancia se encuentra con la precisión. Nuestro equipo de barberos altamente capacitados y apasionados está listo para brindarte un servicio excepcional en cada visita. Desde cortes de cabello hasta tratamientos de barba y peinados, estamos aquí para hacer que tu visión se convierta en realidad. ¡Únete a nosotros y descubre la artesanía que nos define. ¡Tu estilo, nuestra pasión!</p>
-                </div>
-                <div className="Descripcion">
-                    <h2>bBienvenidos a "Nombre de la Barbería" - Tu estilo, nuestro arte</h2><br />
-                    <p>En "Nombre de la Barbería", donde la elegancia se encuentra con la precisión. Nuestro equipo de barberos altamente capacitados y apasionados está listo para brindarte un servicio excepcional en cada visita. Desde cortes de cabello hasta tratamientos de barba y peinados, estamos aquí para hacer que tu visión se convierta en realidad. ¡Únete a nosotros y descubre la artesanía que nos define. ¡Tu estilo, nuestra pasión!</p>
-                </div>
-                <div className="Descripcion">
-                    <h2>cBienvenidos a "Nombre de la Barbería" - Tu estilo, nuestro arte</h2><br />
-                    <p>En "Nombre de la Barbería", donde la elegancia se encuentra con la precisión. Nuestro equipo de barberos altamente capacitados y apasionados está listo para brindarte un servicio excepcional en cada visita. Desde cortes de cabello hasta tratamientos de barba y peinados, estamos aquí para hacer que tu visión se convierta en realidad. ¡Únete a nosotros y descubre la artesanía que nos define. ¡Tu estilo, nuestra pasión!</p>
+           <div className="contenedor">{/*CONTENEDOR PRINCIPAL por si se agg mas datos*/}
+                <div className="Descripcion1">
+                    <h2>{barbeOps[currentIndex1]}</h2>
+                    <p>{descriptions[currentIndex]}</p>
                 </div>
             </div>
             <div>
-            
                 <input type='date' value={fechaCita} onChange={(e) => {setFechaCita(e.target.value);}}/>
                 {fechaCita}{/*muestra la fecha pa probar */}
             </div>
